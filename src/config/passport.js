92bbbba5dd38b58a3998c22d6a3aa8f8da5bf897
Mona Lisa <plong12102001@gmail.com>
2025-01-10 16:50:43 +0700

    new update

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "http://localhost:8080/auth/google/callback",
     },
     async function (_, _, profile, cb) {
       try {
